c275618ff16e55f26ca4a2da22b6412131a4cfb9
Jeongbin Park <pjb7687@gmail.com>
2020-12-01 03:27:17 +0100

    Added experimental watershed segmentation feature

--- ssam/_analysis.py
+++ ssam/_analysis.py
@@ -1282,3 +1282,64 @@ class SSAMAnalysis(object):
             sparel[idx, :] = np.sum(self.dataset.celltype_binned_counts[ct_centers == idx], axis=0)
 
         self.dataset.spatial_relationships = preprocessing.normalize(sparel, axis=1, norm='l1')
+
+    def run_watershed(self):
+        """
+        Run watershed segmentation based on the cell-type map (experimental).
+        """
+        import cv2
+
+        self.watershed_segmentations = np.zeros(self.dataset.vf_norm.shape[:-1], dtype="int32")
+        self.waterhsed_celltype_map = np.zeros(self.dataset.vf_norm.shape[:-1], dtype="int32")
+
+        vn = self.dataset.vf_norm.compute()
+
+        from skimage import filters
+        from skimage import exposure
+
+        for idx, cl in enumerate(denovo_labels_final):
+            m = np.logical_and(self.dataset.celltype_maps == list(denovo_labels_final).index(cl), self.dataset.max_correlations > 0.6, vn > self.dataset.norm_threshold)
+            
+            thresh = np.zeros_like(dapi_thresh_im, dtype=np.uint8)
+            thresh[m] = dapi_thresh_im[m]
+            thresh = thresh[..., 0]
+
+            # noise removal
+            kernel = np.ones((3,3),np.uint8)
+            opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+
+            # sure background area
+            sure_bg = cv2.dilate(opening, kernel, iterations=5)
+
+            # Finding sure foreground area
+            dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+            ret, sure_fg = cv2.threshold(dist_transform, 0, 255, 0)
+
+            # Finding unknown region
+            sure_fg = np.uint8(sure_fg)
+            unknown = cv2.subtract(sure_bg, sure_fg)
+
+            # Marker labelling
+            ret, markers = cv2.connectedComponents(sure_fg)
+
+            # Add one to all labels so that sure background is not 0, but 1
+            markers = markers+1
+
+            # Now, mark the region of unknown with zero
+            markers[unknown==255] = 0
+            
+            if self.dataset.max_correlations is not None:
+                grey = np.zeros_like(self.dataset.max_correlations)
+                grey[m] = self.dataset.max_correlations[m]
+            else:
+                grey = np.zeros_like(self.dataset.max_probabilities)
+                grey[m] = self.dataset.max_probabilities[m]
+            grey = (grey[..., 0] * 255 / np.max(grey)).astype(np.uint8)
+            img = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
+            
+            markers = cv2.watershed(img, markers)
+            
+            markers[markers < 2] = 0
+            markers[markers > 0] -= 1
+            self.waterhsed_celltype_map[markers > 0] = idx
+            self.watershed_segmentations += markers
