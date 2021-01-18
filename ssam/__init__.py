85a2acdc1a68d67c964ca90f58f4743c6e8c4582
Jeongbin Park <jeongbin.park@charite.de>
2021-01-18 22:18:53 +0100

    merged develop mean-var plot

--- ssam/__init__.py
+++ ssam/__init__.py
@@ -79,11 +79,10 @@ def _fast_gaussian_kde(args):
     
     return pd
 
-def run_sctransform(data, **kwargs):
+def run_sctransform(data, clip_range=None, verbose=True, debug_path=None, plot_model_pars=False, **kwargs):
     """
     Run 'sctransform' R package and returns the normalized matrix and the model parameters.
     Package 'feather' is used for the data exchange between R and Python.
-
     :param data: N x D ndarray to normlize (N is number of samples, D is number of dimensions).
     :type data: numpy.ndarray
     :param kwargs: Any keyword arguments passed to R function `vst`.
@@ -91,21 +90,72 @@ def run_sctransform(data, **kwargs):
         (1) normalized N x D matrix.
         (2) determined model parameters.
     """
+    def _log(m):
+        if verbose:
+            print(m)
+            
     vst_options = ['%s = "%s"'%(k, v) if type(v) is str else '%s = %s'%(k, v) for k, v in kwargs.items()]
     if len(vst_options) == 0:
         vst_opt_str = ''
     else:
         vst_opt_str = ', ' + ', '.join(vst_options)
     with TemporaryDirectory() as tmpdirname:
+        if debug_path:
+            tmpdirname = debug_path
         ifn, ofn, pfn, rfn = [os.path.join(tmpdirname, e) for e in ["in.feather", "out.feather", "fit_params.feather", "script.R"]]
-        df = pd.DataFrame(data, columns=[str(e) for e in range(data.shape[1])])
-        df.to_feather(ifn)
-        rcmd = 'library(feather); library(sctransform); mat <- t(as.matrix(read_feather("{0}"))); colnames(mat) <- 1:ncol(mat); res <- sctransform::vst(mat{1}); write_feather(as.data.frame(t(res$y)), "{2}"); write_feather(as.data.frame(res$model_pars_fit), "{3}");'.format(ifn, vst_opt_str, ofn, pfn)
+        _log("Writing temporary files...")
+        if isinstance(data, pd.DataFrame):
+            df = data
+        else:
+            df = pd.DataFrame(data, columns=[str(e) for e in range(data.shape[1])])
+        if version.parse(pyarrow.__version__) >= version.parse("1.0.0"):
+            df.to_feather(ifn, version=1)
+        else:
+            df.to_feather(ifn)
+        rcmd = 'library(arrow); library(sctransform); mat <- t(as.matrix(read_feather("{0}"))); colnames(mat) <- 1:ncol(mat); res <- vst(mat{1}, return_gene_attr=TRUE, return_cell_attr=TRUE); write_feather(as.data.frame(t(res$y)), "{2}"); write_feather(as.data.frame(res$model_pars_fit), "{3}");'.format(ifn, vst_opt_str, ofn, pfn)
+        if plot_model_pars:
+            plot_path = os.path.join(tmpdirname, 'model_pars.png')
+            rcmd += 'png(file="%s", width=3600, height=1200, res=300); plot_model_pars(res, show_var=TRUE); dev.off();'%plot_path
         rcmd = rcmd.replace('\\', '\\\\')
         with open(rfn, "w") as f:
             f.write(rcmd)
-        subprocess.check_output("Rscript " + rfn, shell=True)
-        return pd.read_feather(ofn), pd.read_feather(pfn)
+        _log("Running scTransform via Rscript...")
+        proc = subprocess.Popen(["Rscript", rfn], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while not proc.poll():
+            c = proc.stdout.read(1)
+            if not c:
+                break
+            if verbose:
+                try:
+                    sys.stdout.write(c.decode("utf-8"))
+                except:
+                    pass
+            time.sleep(0.0001)
+        _log("Reading output files...")
+        o, p = pd.read_feather(ofn), pd.read_feather(pfn)
+        if plot_model_pars:
+            try:
+                from matplotlib.image import imread
+                import matplotlib.pyplot as plt
+                img = imread(plot_path)
+                dpi = 80
+                fig = plt.figure(figsize=(img.shape[1]/dpi, img.shape[0]/dpi), dpi=dpi)
+                plt.imshow(img, interpolation='nearest')
+                plt.gca().set_axis_off()
+                plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+                plt.margins(0, 0)
+                plt.gca().xaxis.set_major_locator(plt.NullLocator())
+                plt.gca().yaxis.set_major_locator(plt.NullLocator())
+                plt.show()
+            except:
+                print("Warning: plotting failed, perhaps matplotlib is not available?")
+        _log("Clipping residuals...")
+        if clip_range is None:
+            r = np.sqrt(data.shape[0]/30.0)
+            clip_range = (-r, r)
+        o.clip(*clip_range)
+        return o, p
+
 
 class SSAMDataset(object):
     """
